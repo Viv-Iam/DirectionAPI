dfae893d5344cb0c0577dbbb33c30a38b833b9e8
viv-iam <vivian.opondoh@gmail.com>
2017-07-06 11:11:44 +0300

    Initial commit

---
+++ app/src/main/java/com/innovach/directionapi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.innovach.directionapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
